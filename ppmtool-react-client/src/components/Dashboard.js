05201295831569090c2a4cb48f3b2aa673d724b9
Sheriff <adebisisheriff572@gmail.com>
2020-05-02 21:59:02 +0100

    Adding the first Component

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="mr-1">Welcome to Dashbaord</h1>
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
