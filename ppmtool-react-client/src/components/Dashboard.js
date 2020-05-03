2ae25433526aebdba2a86ba0544c162fd7f8ae74
Sheriff <adebisisheriff572@gmail.com>
2020-05-03 01:41:02 +0100

    Adding Project and Navbar Layout Components

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
                 <h1 className="mr-1">Welcome to Dashbaord</h1>
+                  <ProjectItem />
             </div>
         )
     }
---
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,17 @@
+ import React,{ Component } from 'react'
+
+ class ProjectItem extends Component{
+   
+     render(){
+          return(
+                <div>
+                  <h1>Project item</h1>
+                </div>
+          )
+     }
+ }
+
+export default ProjectItem;
+
+
+ 
