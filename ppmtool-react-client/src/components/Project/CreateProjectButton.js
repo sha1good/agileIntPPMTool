501f2862b76154cb0ad47517bc8f71801572ddf0
Sheriff <adebisisheriff572@gmail.com>
2020-05-03 05:10:43 +0100

    Add Project Component and Controll Form

--- ppmtool-react-client/src/components/Project/CreateProjectButton.js
+++ ppmtool-react-client/src/components/Project/CreateProjectButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const createProjectButton =(props) =>{
+const createProjectButton =() =>{
 
      return(
          <React.Fragment>
